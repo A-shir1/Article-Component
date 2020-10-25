4a59c56ce1155b4c40b17c96958cfcfd2cb84a41
A-shir1 <ma18u10329@btech.nitdgp.ac.in>
2020-10-25 22:03:48 +0530

    Linked to my profile

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 var shareButt = document.querySelector(".share");
-var display =  document.querySelectorAll("#select");
+var displaylg = document.querySelectorAll(".selectlg");
+var displaysm = document.querySelectorAll(".selectsm");
+// Create a media condition that targets viewports at least 768px wide
+const mediaQuery = window.matchMedia('(max-width: 705px)')
 
-shareButt.addEventListener("click", function(){
-    for(var i = 0; i <= display.length; i++)
-        display[i].classList.toggle("disp");
-});
+// Check if the media query is true
+if (mediaQuery.matches) {
+    shareButt.addEventListener("click", function(){
+        for(var i = 0; i <= displaysm.length; i++)
+            displaysm[i].classList.toggle("disp");
+    });
+}
+else{
+    shareButt.addEventListener("click", function(){
+        for(var i = 0; i <= displaylg   .length; i++)
+            displaylg[i].classList.toggle("disp");
+    });
+}
+
 
